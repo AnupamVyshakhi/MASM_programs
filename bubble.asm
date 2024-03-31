2747d6e3143790103302f8c31999e51ea0d9b90c
Anupam Vyshakhi S <115809998+AnupamVyshakhi@users.noreply.github.com>
2024-03-31 20:01:44 +0530

    Add files via upload

--- bubble.asm
+++ bubble.asm
@@ -1,11 +1,11 @@
 data segment
      msg1 db 0ah,0dh,"Enter the limit $"
      msg2 db 0ah,0dh,"Enter the numbers $"
-     msg3 db 0ah,0dh,"The sorted array is $"
+     msg3 db 0ah,0dh,"Enter the sorted array $"
      n db ?
      n1 db 09h dup(?)
 data ends
-code segement
+code segment
      assume cs:code,ds:data
      start:mov ax,data
            mov ds,ax
@@ -18,38 +18,59 @@ code segement
            int 21h
            sub al,30h
            mov n,al
-
-           mov cl,n
-
-           lea dx,msg2
+          
+          lea dx,msg2
            mov ah,09h
            int 21h
 
+           mov cl,n
+
            mov si,offset n1
 
-    read:mov ah,01h
-         int 21h
-         sub al,30h
-         mov [si],al
-         inc si
-         inc cl
-         jnz read
-         mov cl,n
-    loop1:mov ch,n
-          mov si,offset n1
+     scan:mov ah,01h
+          int 21h
+          sub al,30h
+          mov [si],al
+          inc si
+          dec cl
+          jnz scan
+          
+          mov cl,n
 
-    loop2:mov dl,[si]
-          cmp dl,[si+1]
-          jc swap
-          jmp swapped
+     loop1:mov ch,n
+           mov si,offset n1
+     loop2:mov dl,[si]
+           cmp dl,[si+1]
+           jc swap
+           jmp swapped
 
-    swap:mov dl,[si]
-         xchg dl,[si+1]
-         mov [si],dl
+     swap:mov dl,[si]
+          xchg dl,[si+1]
+          mov [si],dl
 
    swapped:inc si
            dec ch
            jnz loop2
-
            dec cl
            jnz loop1
+
+           lea dx,msg3
+           mov ah,09h
+           int 21h
+
+           mov cl,n
+           mov si,offset n1
+
+    print:mov dl,[si]
+           add dl,30h
+           mov ah,02h
+           int 21h
+           inc si
+           dec cl
+           jnz print
+
+           mov ah,4ch
+           int 21h
+code ends
+end start
+
